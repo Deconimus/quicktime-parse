6112b3631bc76123b4029ebf6114bdc0a16e47ae
Deconimus <pascal.sielski@gmx.de>
2018-08-29 23:02:19 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'quicktimeparse',
+	version = '1.0.0',
+	url = 'https://github.com/Deconimus/quicktime-parse.git',
+	author = 'Deconimus',
+	author_email = '',
+	description = 'A parser for quicktime videos.',
+	packages = find_packages(),
+	install_requires = [],
+)
